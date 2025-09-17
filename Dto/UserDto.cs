fe40e1e1ec1f641c3ff4769bed03d722a912ce28
Patrikas Adomaitis <monstras789@gmail.com>
2025-09-17 18:00:02 +0300

    Added swagger

---
+++ Dto/UserDto.cs
@@ -0,0 +1,8 @@
+namespace Codefolio.API.Dto;
+
+public class UserDto
+{
+    public required string Username { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
